f5968b8daaf50215d701357d0a9423f741d64469
Keith Ma <keithchezzy@gmail.com>
2022-12-29 14:48:22 -0800

    search

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,4 +1,16 @@
 function recursiveSearch(arr, target) {
+  // recursive way
+  if (arr.length === 0) {
+    return false;
+  }
+
+  if (arr[0] === target) {
+    return true
+  }
+ 
+ return recursiveSearch(arr.slice(1), target);
+ 
+  
   // type your code here
 }
 
@@ -16,4 +28,14 @@ if (require.main === module) {
 module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
+// start with a if statement that equals to 0 and return false if array isn't found
+// second if statement equals to the target number from the array and returns true if array is found.
 // And a written explanation of your solution
+
+
+ // for (const value of arr){
+  //   if (value === target){
+  //     return true;
+  //   }
+  // }
+  // return false
